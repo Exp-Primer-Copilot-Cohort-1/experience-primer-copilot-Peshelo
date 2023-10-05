7224401b23c242cf1c42635e4586a128b3514716
Peshelo <125571647+Peshelo@users.noreply.github.com>
2023-10-05 14:17:25 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,17 @@
+// Create web server
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+var user = require('./routes/user');
+var comment = require('./routes/comment');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/user', user);
+app.use('/comment', comment);
+
+app.listen(3000);
+console.log("Server running at http://localhost:3000/");git add comments.js
